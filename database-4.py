2a7ba5f2dd668063ab170e76b656b3b1df67ab36
kaaskyah <hayk12255256hayk@gmail.com>
2023-08-22 22:15:51 +0700

    database

---
+++ database-4.py
@@ -0,0 +1,62 @@
+import telebot
+import sqlite3
+
+bot = telebot.TeleBot('6473370618:AAG2VOUwsOSePUMC4ggTzi48t3SmT2CCJXw')
+name = None
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    conn = sqlite3.connect('bot-data.sql')
+    cur = conn.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_incriment primary key, name varchar(50), pass varchar(50))')
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    bot.send_message(message.chat.id, 'Привет, сейчас тебя зарегестрируем! Введите ваше имя')
+    bot.register_next_step_handler(message, user_name)
+
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id, 'Привет, сейчас тебя зарегестрируем! Введите пароль')
+    bot.register_next_step_handler(message, user_pass)
+
+
+def user_pass(message):
+    password = message.text.strip()
+
+    conn = sqlite3.connect('bot-data.sql')
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO users (name, pass) VALUES ('%s', '%s')" % (name, password))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton('Список пользователей', callback_data='users'))
+    bot.send_message(message.chat.id, 'Пользователь зарегестрирован!', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    conn = sqlite3.connect('bot-data.sql')
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM users')
+    users = cur.fetchall()
+
+    info = ''
+    for el in users:
+        info += f'Имя: {el[1]}, пароль: {el[2]}\n'
+
+    cur.close()
+    conn.close()
+
+    bot.send_message(call.message.chat.id, info)
+
+bot.polling(none_stop=True)
